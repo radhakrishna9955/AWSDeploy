6e6614c785ddb86ff7ce8ac26e427bb6ec0f0802
radhakrishna9955 <radhakrishnagandepalli@gmail.com>
2026-02-26 11:59:20 +0530

    update

--- src/main/java/com/example/demo/CseController.java
+++ src/main/java/com/example/demo/CseController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CseController {
 	@GetMapping("/adityacse")
 	public String rocks() {
-		return "welcome to aditya cse department";
+		return "radha krishna is devops enginner";
 	}
 	@GetMapping("/adityait")
 		public String rocks1() {
